bf755e2a4842bb135ea873441a74bd07b05e825f
atonus <kappegit@valle.fi>
2021-09-29 09:46:53 +0300

    fix path

--- update_sensordata.sh
+++ update_sensordata.sh
@@ -6,6 +6,6 @@ curl -X POST https://content.dropboxapi.com/2/files/download\
      --header "Authorization: Bearer $DROPBOX_AUTH_TOKEN"\
      --header "Dropbox-API-Arg: {\"path\": \"/container.tar.gz\"}" > ~/kappe/container.tar.gz
 tar -xvzf ~/kappe/container.tar.gz -C ~/kappe
-mv ~/kappe/home/pi/sensor_logs/* ~/kappe/sensor_logs
+mv ~/kappe/home/pi/sensorit/sensor_logs/* ~/kappe/sensor_logs
 rm ~/kappe/container.tar.gz
 rm -r ~/kappe/home
