afb5ced41ca15d93a11446111a76f44469ca0728
atonus <kappegit@valle.fi>
2023-06-03 14:22:21 +0300

    add soil_moisture with smoothing

--- graph.py
+++ graph.py
@@ -3,6 +3,7 @@
 # sudo pip3 install dash dash-renderer dash-html-components dash-core-components plotly
 # pip install --upgrade pip
 
+import math
 import dash
 from dash import dcc
 from dash import html
@@ -13,16 +14,18 @@ import os.path
 from os import path
 import glob
 import subprocess
+import pandas as pd
 
 app = dash.Dash()
 utc_offset = int(str(datetime.now(timezone.utc).astimezone().utcoffset()).split(':')[0])
 utc_offset = 0 #override
 
-names = ['kuisti', 'saunan_putket', 'keittion_putket', 'vessan_putket', 'sisalla', 'vessa', 'kasvihuone']
+names = ['kuisti', 'saunan_putket', 'keittion_putket', 'vessan_putket', 'sisalla', 'vessa', 'kasvihuone', 'multa']
 timestamps = {}
 temperatures = {}
 humidities = {}
 battery_voltages = {}
+soil_moistures = {}
 
 x = requests.get('http://valle.fi/graph_day_range.php')
 range = 2
@@ -58,16 +61,23 @@ for name in names:
 	temperatures[name + "_raw"] = []
 	humidities[name + "_raw"] = []
 	battery_voltages[name + "_raw"] = []
+	soil_moistures[name + "_raw"] = []
 
 	for logdate in logdates:
 		if path.exists("/home/pi/kappe/sensor_logs/" + logdate + "_" + name + ".json"):
 			with open("/home/pi/kappe/sensor_logs/" + logdate + "_" + name + ".json") as f:
 				y = json.load(f)
-				for x in y:
-					timestamps[name + "_raw"].append(x['timestamp'])
-					temperatures[name + "_raw"].append(x['temperature'])
-					humidities[name + "_raw"].append(x['humidity'])
-					battery_voltages[name + "_raw"].append(x['battery'])
+				if name is 'multa':
+					for x in y:
+						timestamps[name + "_raw"].append(x['timestamp'])
+						temperatures[name + "_raw"].append(x['temp'])
+						soil_moistures[name + "_raw"].append(x['moisture'])
+				else:
+					for x in y:
+						timestamps[name + "_raw"].append(x['timestamp'])
+						temperatures[name + "_raw"].append(x['temperature'])
+						humidities[name + "_raw"].append(x['humidity'])
+						battery_voltages[name + "_raw"].append(x['battery'])
 
 #
 # Set value arrays
@@ -77,21 +87,31 @@ for name in names:
 	temperatures[name] = []
 	humidities[name] = []
 	battery_voltages[name] = []
+	soil_moistures[name] = []
 	i = 0
 	for kellostamp in timestamps['kello']:
 		if kellostamp in timestamps[name + "_raw"]:
 			date_time_obj = datetime.strptime(timestamps[name + "_raw"][i], '%d.%m.%Y %H:%M:%S') + timedelta(hours = utc_offset)
 			timestamps[name].append(date_time_obj)
 			temperatures[name].append(temperatures[name + "_raw"][i])
-			humidities[name].append(humidities[name + "_raw"][i])
-			battery_voltages[name].append(battery_voltages[name + "_raw"][i])
+			if name is 'multa':
+				soil_moistures[name].append(soil_moistures[name + "_raw"][i])
+			else:
+				humidities[name].append(humidities[name + "_raw"][i])
+				battery_voltages[name].append(battery_voltages[name + "_raw"][i])
 			i += 1
 		else:
 			date_time_obj = datetime.strptime(kellostamp, '%d.%m.%Y %H:%M:%S') + timedelta(hours = utc_offset)
 			timestamps[name].append(kellostamp)
 			temperatures[name].append("None")
-			humidities[name].append("None")
-			battery_voltages[name].append("None")
+			if name is 'multa':
+				soil_moistures[name].append("None")
+			else:
+				humidities[name].append("None")
+				battery_voltages[name].append("None")
+
+df = pd.DataFrame(soil_moistures['multa'], columns=['data'])
+soil_moistures['multa_smooth'] = df['data'].rolling(window=12).mean()
 
 app.layout = html.Div(children=[
 	html.H1(children='Ruuvidata'),
@@ -106,6 +126,7 @@ app.layout = html.Div(children=[
 				{'x': timestamps['vessan_putket'], 'y': temperatures['vessan_putket'], 'type': 'line', 'name': 'Vessan putket'},
 				{'x': timestamps['vessa'], 'y': temperatures['vessa'], 'type': 'line', 'name': 'Vessa'},
 				{'x': timestamps['kasvihuone'], 'y': temperatures['kasvihuone'], 'type': 'line', 'name': 'Kasvihuone'},
+				{'x': timestamps['multa'], 'y': temperatures['multa'], 'type': 'line', 'name': 'Multa'},
 			],
 			'layout': {
 				'title': 'Lämpötilat',
@@ -114,6 +135,25 @@ app.layout = html.Div(children=[
 				},
 				'yaxis':{
 					 'title':'°C'
+				},
+				'hovermode': 'x'
+			}
+		}
+	),
+	dcc.Graph(
+    	id='soil_moistures',
+		figure={
+			'data': [
+				{'x': timestamps['multa'], 'y': soil_moistures['multa_smooth'], 'type': 'line', 'name': 'Multa'},
+			],
+			'layout': {
+				'title': 'Mullan kosteus',
+				'xaxis':{
+					'title':''
+				},
+				'yaxis':{
+					'title':'?',
+					'range': [int(soil_moistures['multa_smooth'].tail(1).values[0] - 50), math.ceil(soil_moistures['multa_smooth'].max())]
 				},
 				'hovermode': 'x'
 			}
